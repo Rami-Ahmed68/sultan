3dcd5c6a287b7762b5e64807a53f4cad12a2cc9d
ramiahmed68 <ramiahmet68@gmail.com>
2025-01-06 23:59:52 +0200

    test

--- src/router/auth/get.js
+++ src/router/auth/get.js
@@ -13,58 +13,9 @@ router.get("/:who_want", async (req, res, next) => {
     // find the admin
     const admin = await User.find();
 
-    // create list of admin's data
-    let admin_data;
-
-    // check who want the admin data
-    if (req.params.who_want == "admin") {
-      // get to the all admin's data without password
-      admin_data = _.pick(admin[0], [
-        "_id",
-        "avatar",
-        "cv",
-        "english_name",
-        "english_description",
-        "english_bio",
-        "arabic_name",
-        "arabic_description",
-        "arabic_bio",
-        "email",
-        "phon",
-        "whatsapp",
-        "telegram",
-        "facebook",
-        "instgram",
-        "linkedIn",
-        "behance",
-        "email_address",
-        "skills_page_status",
-      ]);
-    } else {
-      // get to the all admin's data without password and email and id
-      admin_data = _.pick(admin[0], [
-        "avatar",
-        "cv",
-        "english_name",
-        "english_description",
-        "english_bio",
-        "arabic_name",
-        "arabic_description",
-        "arabic_bio",
-        "phon",
-        "whatsapp",
-        "telegram",
-        "facebook",
-        "instgram",
-        "linkedIn",
-        "behance",
-        "email_address",
-      ]);
-    }
-
     // create response
     const response = {
-      admin_data: admin_data,
+      admin_data: admin[0],
     };
 
     // send the response to the clint
